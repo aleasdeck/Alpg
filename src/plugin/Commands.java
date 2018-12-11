98297740c4aa9392d1d15699cc112dcfd3018249
Aleasdeck <aleasdeck@gmail.com>
2018-12-12 01:12:52 +1000

    Эта хуйня не работает

--- src/plugin/Commands.java
+++ src/plugin/Commands.java
@@ -24,23 +24,15 @@ public class Commands implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) return true;
 
-        int[][] maze = {
-                {1, 1, 1, 1, 1},
-                {0, 0, 1, 0, 1},
-                {1, 1, 1, 0, 1},
-                {0, 0, 0, 0, 1},
-                {1, 1, 1, 1, 1},
-        };
+        int[][] maze = generateMaze(20);
 
         Location start = ((Player) sender).getLocation();
         start.setX(((Player) sender).getLocation().getX());
         start.setY(((Player) sender).getLocation().getY() - 1);
         start.setZ(((Player) sender).getLocation().getZ());
 
-        //Location forcycle = start;
-
-        for (int row = 0; row < 5; row++)
-            for (int column = 0; column < 5; column++)
+        for (int row = 0; row < maze.length; row++)
+            for (int column = 0; column < maze.length; column++)
             {
                 start.setX(start.getX() + row);
                 start.setZ(start.getZ() + column);
@@ -60,21 +52,11 @@ public class Commands implements CommandExecutor {
         int[][] maze = new int[size][size];
         int cellsCount = 0;
 
-        // Генерация пустого лабиринта с клетками
-        for(int row = 0; row < size; row++)
-            for(int column = 0; column < size; column++)
-            {
-                if(row % 2 == 0) {
-                    if (column % 2 == 0) {
-                        maze[row][column] = 0;
-                        cellsCount++;
-                    }
-                    else maze[row][column] = 0;
-                }
-                else
-                    maze[row][column] = 0;
-            }
+        // Определяем количество ячеек
+        if(size % 2 != 0) cellsCount = (size + 1) / 4;
+        else cellsCount = size / 4;
 
+        // Строим лабиринт и рушим стены
         breakTheWall(maze, cellsCount, size);
 
         return maze;
@@ -84,40 +66,54 @@ public class Commands implements CommandExecutor {
 
         Random random = new Random();
         int nowPos = 0, fullCellsCount = 1;
+        emptyMaze[0][0] = 1;
 
-        mazePosition[] position = null;
-        position[0] = new mazePosition(0,0);
+        mazePosition[] position = new mazePosition[cellsCount];
+        position[0] = new mazePosition();
 
-        while(nowPos < cellsCount){
+        while(fullCellsCount < cellsCount){
+            System.out.println(">------<" + fullCellsCount);
             if(hasFreeCell(emptyMaze, position[nowPos])){
-                switch(random.nextInt(4)){
+                switch(random.nextInt(5)){
                     case 1: { // x-
+                        System.out.println(">>> 1 Вниз");
+                        if(position[nowPos].x - 1 < 0) break;
                         emptyMaze[position[nowPos].x - 1][position[nowPos].y] = 1;
                         emptyMaze[position[nowPos].x - 2][position[nowPos].y] = 1;
                         nowPos++;
                         fullCellsCount++;
                         position[nowPos] = new mazePosition(position[nowPos - 1].x - 2, position[nowPos - 1].y);
+                        break;
                     }
                     case 2: { // x+
+                        System.out.println(">>> 2 Вверх");
+                        if(position[nowPos].x + 1 > size) break;
                         emptyMaze[position[nowPos].x + 1][position[nowPos].y] = 1;
                         emptyMaze[position[nowPos].x + 2][position[nowPos].y] = 1;
                         nowPos++;
                         fullCellsCount++;
                         position[nowPos] = new mazePosition(position[nowPos - 1].x + 2, position[nowPos - 1].y);
+                        break;
                     }
                     case 3: { // y-
+                        System.out.println(">>> 3 Налево");
+                        if(position[nowPos].y - 1 < 0) break;
                         emptyMaze[position[nowPos].x][position[nowPos].y - 1] = 1;
                         emptyMaze[position[nowPos].x][position[nowPos].y - 2] = 1;
                         nowPos++;
                         fullCellsCount++;
                         position[nowPos] = new mazePosition(position[nowPos - 1].x, position[nowPos - 1].y - 2);
+                        break;
                     }
                     case 4: { // y+
+                        System.out.println(">>> 4 Направо");
+                        if(position[nowPos].y + 1 > size) break;
                         emptyMaze[position[nowPos].x][position[nowPos].y + 1] = 1;
                         emptyMaze[position[nowPos].x][position[nowPos].y + 2] = 1;
                         nowPos++;
                         fullCellsCount++;
                         position[nowPos] = new mazePosition(position[nowPos - 1].x, position[nowPos - 1].y + 2);
+                        break;
                     }
                 }
             }
@@ -140,6 +136,10 @@ public class Commands implements CommandExecutor {
     }
 
     private class mazePosition{
+
+        public mazePosition () {
+
+        }
 
         public int x;
         public int y;
